59814890d53aba30a6973099d9488077bf9b21ec
littlehorseboy <littlehorseboy@gmail.com>
2019-07-09 23:26:16 +0800

    feat: WorkCountdownMain.tsx, AddNewMission.tsx

---
+++ src/components/UI/AddNewMission/AddNewMission.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles(createStyles({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+    borderRadius: 0,
+    '& > input::placeholder': {
+      color: '#FF4384',
+      fontWeight: 'bold',
+      fontStyle: 'italic',
+    },
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
+export default function CustomizedInputBase(): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="add a new mission…"
+        inputProps={{ 'aria-label': 'add a new mission…' }}
+      />
+      <IconButton className={classes.iconButton} aria-label="Search">
+        <AddIcon color="secondary" />
+      </IconButton>
+    </Paper>
+  );
+}
--- src/components/WorkCountdownMain/WorkCountdownMain.tsx
+++ src/components/WorkCountdownMain/WorkCountdownMain.tsx
@@ -4,16 +4,64 @@ import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import AddNewMission from '../UI/AddNewMission/AddNewMission';
 
-const useStyles = makeStyles((theme: Theme): Record<'root' | 'bgPink' | 'bgBlue', CSSProperties | (() => CSSProperties)> => createStyles({
+const useStyles = makeStyles((theme: Theme): Record<
+'root' | 'leftPane' | 'middlePane' | 'middleContaiberPane' | 'middleLeftPane' | 'middleRightPane' |
+'rightPane' | 'fabContainer' | 'fab', CSSProperties | (() => CSSProperties)
+> => createStyles({
   root: {
     padding: 0,
+    minHeight: '100vh',
+    display: 'flex',
   },
-  bgPink: {
+  leftPane: {
+    backgroundColor: '#FFEDF7',
+    '& > div': {
+      paddingLeft: 85,
+      paddingRight: 30,
+      paddingTop: 48,
+      paddingBottom: 48,
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+  },
+  middlePane: {
+    position: 'relative',
+  },
+  middleContaiberPane: {
+    height: '100%',
+  },
+  middleLeftPane: {
     backgroundColor: '#FFEDF7',
   },
-  bgBlue: {
+  middleRightPane: {
     backgroundColor: '#003164',
+  },
+  rightPane: {
+    backgroundColor: '#003164',
+  },
+  fabContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fab: {
+    width: 540,
+    height: 540,
+    border: '3px solid transparent',
+    outline: '3px solid transparent',
   },
 }));
 
@@ -23,35 +71,34 @@ export default function WorkCountdownMain(): JSX.Element {
   return (
     <Container maxWidth={false} className={classes.root}>
       <Grid container spacing={0}>
-        <Grid item xs={12} sm={5} className={classes.bgPink}>
-          <div>add a new mission…</div>
-          <div>the First thing to do today</div>
-          <div>25:00</div>
-          <div>the second thing to do today</div>
-          <div>the third thing to do today</div>
-          <div>the forth thing to do today</div>
+        <Grid item xs={12} sm={5} className={classes.leftPane}>
+          <div>
+            <AddNewMission />
+            <div>
+              <FormControlLabel
+                control={<Checkbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} value="checkedH" />}
+                label="Custom icon"
+              />
+              <div>the First thing to do today</div>
+              <div>25:00</div>
+            </div>
+            <div>
+              <div>the second thing to do today</div>
+              <div>the third thing to do today</div>
+              <div>the forth thing to do today</div>
+            </div>
+          </div>
         </Grid>
-        <Grid item xs={12} sm={5}>
-          <Grid container spacing={0}>
-            <Grid item xs={12} sm={6} className={classes.bgPink}>
-              <div>add a new mission…</div>
-              <div>the First thing to do today</div>
-              <div>25:00</div>
-              <div>the second thing to do today</div>
-              <div>the third thing to do today</div>
-              <div>the forth thing to do today</div>
-            </Grid>
-            <Grid item xs={12} sm={6} className={classes.bgBlue}>
-              <div>add a new mission…</div>
-              <div>the First thing to do today</div>
-              <div>25:00</div>
-              <div>the second thing to do today</div>
-              <div>the third thing to do today</div>
-              <div>the forth thing to do today</div>
-            </Grid>
+        <Grid item xs={12} sm={5} className={classes.middlePane}>
+          <Grid container spacing={0} className={classes.middleContaiberPane}>
+            <Grid item xs={12} sm={6} className={classes.middleLeftPane}></Grid>
+            <Grid item xs={12} sm={6} className={classes.middleRightPane}></Grid>
           </Grid>
+          <div className={classes.fabContainer}>
+            <Fab className={classes.fab} color="secondary" />
+          </div>
         </Grid>
-        <Grid item xs={12} sm={2} className={classes.bgBlue}>
+        <Grid item xs={12} sm={2} className={classes.rightPane}>
         </Grid>
       </Grid>
     </Container>
